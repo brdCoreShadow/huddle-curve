42477828041a2c183c5ab0e5ce9e953324807c95
Oleksandr-Berd <alex.berd86@gmail.com>
2025-06-30 15:37:44 +0300

    about mob is done

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,43 +8,43 @@ const About: React.FC = () => {
   return (
     <SC.AboutList>
       <SC.AboutItem>
-        <div>
+        <SC.AboutPicThumb>
           <img src={growPic} alt="grow" />
-        </div>
-        <div>
+        </SC.AboutPicThumb>
+        <SC.AboutContentCon>
           <h4>Grow Together</h4>
           <p>
             Generate meaningful discussions with your audience and build a
             strong, loyal community. Think of the insightful conversations you
             miss out on with a feedback form.
           </p>
-        </div>
+        </SC.AboutContentCon>
       </SC.AboutItem>
       <SC.AboutItem>
-        <div>
+        <SC.AboutPicThumb>
           <img src={conversationsPic} alt="Conversations" />
-        </div>
-        <div>
+        </SC.AboutPicThumb>
+        <SC.AboutContentCon>
           <h4>Flowing Conversations</h4>
           <p>
             You wouldn't paginate a conversation in real life, so why do it
             online? Our threads have just-in-time loading for a more natural
             flow.
           </p>
-        </div>
+        </SC.AboutContentCon>
       </SC.AboutItem>
       <SC.AboutItem>
-        <div>
+        <SC.AboutPicThumb>
           <img src={usersPic} alt="uesers" />
-        </div>
-        <div>
+        </SC.AboutPicThumb>
+        <SC.AboutContentCon>
           <h4>Your Users</h4>
           <p>
             It takes no time at all to integrate Huddle with your app's
             authentication solution. This means, once signed in to your app,
             your users can start chatting immediately.
           </p>
-        </div>
+        </SC.AboutContentCon>
       </SC.AboutItem>
     </SC.AboutList>
   );
--- src/components/About/AboutStyled.ts
+++ src/components/About/AboutStyled.ts
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 
 import bgAboutMobTop from "../../assets/images/bgaboutMob.png";
+import bgAboutMobBottom from "../../assets/images/bgMobBottom.png";
 
 export const AboutList = styled.ul``;
 
@@ -11,12 +12,58 @@ export const AboutItem = styled.li`
   text-align: center;
 
   &:nth-child(1) {
-    margin-bottom: 68px;
+    margin-bottom: 24px;
 
     padding-top: 180px;
-    padding-bottom: 132px;
+    padding-bottom: 200px;
 
     background-image: url(${bgAboutMobTop});
-    background-size: contain;
+    background-size: cover;
+  }
+
+  &:nth-child(2) {
+    margin-top: 64px;
+    margin-bottom: 120px;
+  }
+
+  &:nth-child(3) {
+    margin-bottom: 8px;
+
+    padding-top: 80px;
+    padding-bottom: 276px;
+
+    background-image: url(${bgAboutMobBottom});
+    background-size: cover;
   }
 `;
+
+export const AboutPicThumb = styled.div`
+margin-bottom: 40px;
+
+& > img{
+    width: 280px;
+    height: 280px;
+
+    margin-left: auto;
+    margin-right: auto;
+}
+`
+
+export const AboutContentCon = styled.div`
+& > h4{
+    margin-bottom: 16px;
+
+    font-size: 20px;
+    line-height: 1.5;
+    font-weight: 700;
+
+    color: #00252E;
+}
+
+& > p{
+    font-size: 14px;
+    line-height: 1.5;
+
+    color: #00252E;
+}
+`
